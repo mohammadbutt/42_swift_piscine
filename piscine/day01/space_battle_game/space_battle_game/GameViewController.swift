0af5ec230ebb71ddbdb9e41c622dd5b25b4c43e2
Mohammad Butt <mbutt@student.42.us.org>
2019-10-12 22:25:43 -0700

    Updating progress

--- piscine/day01/space_battle_game/space_battle_game/GameViewController.swift
+++ piscine/day01/space_battle_game/space_battle_game/GameViewController.swift
@@ -10,20 +10,24 @@ import UIKit
 import SpriteKit
 import GameplayKit
 
-class GameViewController: UIViewController {
+class GameViewController: UIViewController
+{
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        
-        if let view = self.view as! SKView? {
+        if let view = self.view as! SKView?
+        {
             // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
+//            if let scene = SKScene(fileNamed: "GameScene")
+            let scene = GameScene(size: CGSize(width: 1536, height: 2048))
+//            {
                 // Set the scale mode to scale to fit the window
                 scene.scaleMode = .aspectFill
                 
                 // Present the scene
                 view.presentScene(scene)
-            }
+//            }
             
             view.ignoresSiblingOrder = true
             
@@ -32,24 +36,30 @@ class GameViewController: UIViewController {
         }
     }
 
-    override var shouldAutorotate: Bool {
+    override var shouldAutorotate: Bool
+    {
         return true
     }
 
-    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask
+    {
+        if UIDevice.current.userInterfaceIdiom == .phone
+        {
             return .allButUpsideDown
-        } else {
+        } else
+        {
             return .all
         }
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Release any cached data, images, etc that aren't in use.
     }
 
-    override var prefersStatusBarHidden: Bool {
+    override var prefersStatusBarHidden: Bool
+    {
         return true
     }
 }
