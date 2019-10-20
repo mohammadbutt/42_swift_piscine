df52a7714a0bdd36e6fc55491302c30f192f27ff
Mohammad Butt <maniqrb@gmail.com>
2019-10-20 13:28:06 -0700

    Update ResturantTableViewCell.swift

--- piscine/rush01/resturantApp/resturantApp/Screen/Details/Views/ResturantTableViewCell.swift
+++ piscine/rush01/resturantApp/resturantApp/Screen/Details/Views/ResturantTableViewCell.swift
@@ -5,6 +5,7 @@
 //  Created by Mohammad Butt on 10/18/19.
 //  Copyright © 2019 Mohammad Butt. All rights reserved.
 //
+// Credits: Gary Tokman www.youtube.com/playlist?list=PLgwcD-3WBz9lQ5NgVon61nEiFRoxs5kAT
 
 import UIKit
 import AlamofireImage
