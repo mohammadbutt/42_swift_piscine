ff46c392a38ccff072271df91486b7fa3f9c7d4c
Mohammad Butt <maniqrb@gmail.com>
2019-10-20 13:38:55 -0700

    Update GameViewController.swift

--- piscine/day01/space_battle_game/space_battle_game/GameViewController.swift
+++ piscine/day01/space_battle_game/space_battle_game/GameViewController.swift
@@ -5,6 +5,7 @@
 //  Created by Mohammad Butt on 10/12/19.
 //  Copyright © 2019 Mohammad Butt. All rights reserved.
 //
+// Credits: Matt Heaney Apps www.youtube.com/playlist?list=PLrL5aCF7Ods-6C7QjzXibUZoYjMzhWBfL
 
 import UIKit
 import SpriteKit
